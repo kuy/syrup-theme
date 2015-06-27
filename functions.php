431242947945462c8a3e4ddc5bafa6148b868db4
Yuki Kodama <endflow.net@gmail.com>
2015-06-27 13:48:42 +0900

    Enable featured image

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php
 
+function syrup_setup() {
+    add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 120, 120 );
+}
+add_action( 'after_setup_theme', 'syrup_setup' );
+
 function syrup_theme_scripts() {
     wp_enqueue_style( 'syrup-pure', get_stylesheet_directory_uri() . '/css/pure.css' );
 	wp_enqueue_style( 'syrup-theme-style', get_stylesheet_uri() );
