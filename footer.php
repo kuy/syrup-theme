d15a53bccf4fc749b3c8d5ab9665456c4862eac4
Yuki Kodama <endflow.net@gmail.com>
2015-07-06 23:53:54 +0900

    Put copyright line

--- footer.php
+++ footer.php
@@ -2,6 +2,9 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
+			&copy; <?= date( 'Y' ) ?> maimai travel
+			<span class="sep">|</span>
+			<a href="<?= home_url( '/about' ) ?>">about us</a>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
 
