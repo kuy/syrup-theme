f185913603c4d67ec09c3687386d01735a2f4bed
Yuki Kodama <endflow.net@gmail.com>
2015-07-07 00:18:53 +0900

    Don't show page title at front page

--- content.php
+++ content.php
@@ -2,7 +2,7 @@
 
 	<header class="entry-header">
 		<?php
-			if ( is_single() || is_page() ) :
+			if ( !is_front_page() && ( is_single() || is_page() ) ) :
 				the_title( '<h2 class="entry-title">', '</h2>' );
 			endif;
 		?>
